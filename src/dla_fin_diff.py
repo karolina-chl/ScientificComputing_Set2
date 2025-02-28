2306b88ef6d9a2c9c8ab33472c5dc6cf696cc576
PaulJungnickel <jungnikp@gmail.com>
2025-02-28 13:27:54 +0100

    faster neighbor grid update

--- src/dla_fin_diff.py
+++ src/dla_fin_diff.py
@@ -2,7 +2,6 @@ import numpy as np
 from finite_difference import *
 from utils import *
 
-
 def neighbors_grid(g):
     grid_size = g.shape[-1]
     
@@ -15,15 +14,22 @@ def neighbors_grid(g):
 
 
 @njit
-def grow_g(g, p_g):    
+def grow_g(g, p_g, neighbors):    
+    neighbor_coords = [(0,1),(0,-1),(1,0),(-1,0)]
+    grid_size = g.shape[0]
     x = np.random.uniform(0,1)
     p=0
-    for i, row in enumerate(g):
-        for j, el in enumerate(row):
+    for i in range(grid_size):
+        for j in range(grid_size):
             p += p_g[i,j]
             if p>x:
                 # grow in this cell
                 g[i,j] = 1
+                #update the neighbors grid
+                neighbors[i,j] = 0
+                for dy, dx in neighbor_coords:
+                    if 0<= i+dy < grid_size:
+                        neighbors[i+dy, (j+dx) % grid_size] = 1 - g[i+dy, (j+dx) % grid_size]
                 return
 
 
@@ -33,6 +39,7 @@ def dla_growth(eta, omega, initial_condition, growth_steps=1000, diffusion_toler
     c = np.zeros([growth_steps, grid_size, grid_size])
     g = np.zeros_like(c)
     g[0] = initial_condition
+    neighbors = neighbors_grid(g[0])
     c[0], _,_ = SOR_top_down(c[0], omega, tolerance=diffusion_tolerance, mask=1-g[0])
     for t in range(0, growth_steps-1):
         
@@ -40,19 +47,25 @@ def dla_growth(eta, omega, initial_condition, growth_steps=1000, diffusion_toler
         c[t+1], _,_ = SOR_top_down(c[t].copy(), omega, tolerance=diffusion_tolerance, mask=1-g[t])
         
         g[t+1] = g[t]
-        neighbors = neighbors_grid(g[t+1])
         p_g = neighbors * c[t+1]**eta
         p_g = p_g / np.sum(p_g)
         
         # plot_grid(c[t])
         
-        grow_g(g[t+1], p_g)
+        grow_g(g[t+1], p_g, neighbors)
+        
+        plot_grid(neighbors)
     return g, c
         
     
+# def interval_bisection_omega(f, xlim=[0,2], tol=1e-3):
+#     while xlim[1] - xlim[0] > tol:
+#         dx
+#         center = (xlim[1] + xlim[0]) /2
+#         f_p = f(center + dx)
         
 if __name__ == '__main__':
-    np.random.seed(42)
+    np.random.seed(420)
     grid_size = 100
     initial_cond = np.zeros([grid_size, grid_size])
     initial_cond[-2, grid_size//2] = 1
@@ -63,7 +76,8 @@ if __name__ == '__main__':
     
     g, c = dla_growth(eta, omega, initial_cond, growth_steps=1000)
         
-    plot_grid(c[-1], g[-1])
+    # plot_grid(c[-1], g[-1])
+    plot_animation(c, g)
         
         
     
